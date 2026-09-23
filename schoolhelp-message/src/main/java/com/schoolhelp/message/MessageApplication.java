f58b9b8ca7a5ae080e59127ea9e100f3145d97a6
liumx <1103183246@qq.com>
2026-09-23 08:54:05 +0800

    新建message模块

---
+++ schoolhelp-message/src/main/java/com/schoolhelp/message/MessageApplication.java
@@ -0,0 +1,13 @@
+package com.schoolhelp.message;
+
+/**
+ * Hello world!
+ *
+ */
+public class MessageApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
